7dda95ee789b019ad615ec9036e8a3d383587f35
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2019-12-02 16:45:38 -0300

    added tweet model

---
+++ app/Controllers/Http/TweetController.js
@@ -0,0 +1,73 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with tweets
+ */
+class TweetController {
+  /**
+   * Show a list of all tweets.
+   * GET tweets
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new tweet.
+   * GET tweets/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+
+  async store({ request, response }) {
+  }
+
+  /**
+   * Display a single tweet.
+   * GET tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing tweet.
+   * GET tweets/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+
+  async update({ params, request, response }) {
+  }
+
+  /**
+   * Delete a tweet with id.
+   * DELETE tweets/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {
+  }
+}
+
+module.exports = TweetController
--- comandos.sh
+++ comandos.sh
@@ -2,4 +2,12 @@ npm install --global @adonisjs/cli
 adonis new adonis-api --api-only
 yarn add pg
 adonis migration:run
+
 adonis make:controller --name
+# AuthController
+# AppController
+
+
+# Crud
+adonis make:model Tweet -m -c 
+#-m cria a migration e -c o controller automaticamente
